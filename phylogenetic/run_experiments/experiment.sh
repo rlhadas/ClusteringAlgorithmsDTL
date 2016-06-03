f070a4920af683aaa796ff0bd3da1171ddfc1694
Jean Sung <jsung@hmc.edu>
2016-06-03 16:52:23 -0700

    for final run!!

--- phylogenetic/run_experiments/experiment.sh
+++ phylogenetic/run_experiments/experiment.sh
@@ -2,7 +2,7 @@
 
 ## Before running, must see the following parameters:
 # maximum value for k, parameter from k centers and k medoids 
-MAX_K=2
+MAX_K=4
 
 # Full path of each python file with script
 SCRIPTS=("../cluster/FromNewick.py" "../cluster/FromNewick_random.py" "../cluster/k_centers.py")
@@ -11,25 +11,14 @@ SCRIPTS=("../cluster/FromNewick.py" "../cluster/FromNewick_random.py" "../cluste
 # For the naming of the output directory, leave underscore at the end 
 DESCRIPTIONS=("kmedoids_pointcollect_" "kmedoids_random_" "kcenters_")
 
-#index=0
 START_LOC=$(pwd)
-#INPUT_FILES=("$(echo ../TreeLifeData/*.newick)")
-INPUT_FILES=("../TreeLifeData/COG0001.newick" "../TreeLifeData/COG0002.newick")
-
-# for index in ${!array[*]}; do 
-#   echo "${array[$index]} is in ${array2[$index]}"
-# done
-
-
-#   echo "${array[$index]} is in ${array2[$index]}"
-# done
+INPUT_FILES=("$(echo ../TreeLifeData/*.newick)")
 
 
 for index in ${!SCRIPTS[*]}
 do
 	PYTHON_PATH=${SCRIPTS[$index]}
 	TYPE=${DESCRIPTIONS[$index]}
-	#index=$(let index+=1)
 	echo "Running: $TYPE"
 	echo "For file: $PYTHON_PATH"
 	
@@ -44,8 +33,6 @@ do
 
 
 	# Run specified method on each file for the tree of life data 
-	
-	
 	cd $RUN_DIR
 	for INPUT in $INPUT_FILES
 	do
